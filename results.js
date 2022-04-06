85b9fb2463ad6678bbfb5c17146682d8d78dc4f8
suleman-suraj <sulemansuraj1233@gmail.com>
2022-04-06 15:04:23 +0000

    first commit

---
+++ results.js
@@ -0,0 +1,14 @@
+const results=[
+    {name:"Windy Dari", class:"Form one", marks:8},
+    {name:"Flex Dassah", class:"Form one", marks:7},
+    {name:"Dee Abu", class:"Form one", marks:9},
+    {name:"Rim Adam", class:"Form one", marks:5},
+    {name:"Awudu Dery", class:"Form one", marks:6},
+    {name:"Mak Dari", class:"Form one", marks:8},
+    {name:"Felix Dassah", class:"Form one", marks:7},
+    {name:"Deen Abu", class:"Form one", marks:9},
+    {name:"Rimat Adam", class:"Form one", marks:5},
+    {name:"Awudu Oscar", class:"Form one", marks:6},
+]
+
+module.exports=results
